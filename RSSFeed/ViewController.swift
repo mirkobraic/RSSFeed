c9369b06518d69b7a47d97a648278823bf240cf3
Mirko Braic <mbraic@profico.hr>
2023-12-25 00:25:31 +0100

    Initial Commit

---
+++ RSSFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSSFeed
+//
+//  Created by Mirko Braic on 25.12.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
